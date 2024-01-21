e2d547905d069e735e18c21c8142eadf7f9c8d9b
krisawyerr <krisawyerrr@gmail.com>
2024-01-21 15:58:49 -0800

    added rollup

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
+/* import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+}); */
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['/src/main.jsx'],
+    },
+  },
 });
