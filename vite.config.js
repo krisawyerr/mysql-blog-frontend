6b718f50d4e10f88787363fe8c27222499c818e3
krisawyerr <krisawyerrr@gmail.com>
2024-01-21 16:15:46 -0800

    undid vite config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-/* import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-}); */
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Adjust this based on your deployment path
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['/src/main.jsx'],
-    },
-  },
 });
 
